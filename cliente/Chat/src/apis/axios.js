6d48cdda8de784ffe0427948b10869a5d154190a
Joel <joeletchegaray5@gmail.com>
2025-03-02 15:05:23 -0300

    FUNCIONO MAL EL ANTERIOR CAMBIO, SE UTILIZARON ARRAYS PARA PODER HACER FUNCIONAR LA CONEXION EN PRODUCCION COMO EN DESARROLLO

--- cliente/Chat/src/apis/axios.js
+++ cliente/Chat/src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://chatapp-production-b82e.up.railway.app/api' || "http://localhost:3000",
+    baseURL: ['https://chatapp-production-b82e.up.railway.app/api' , "http://localhost:3000"] ,
     withCredentials: true
 })
 
--- cliente/Chat/src/pages/PagesChat.jsx
+++ cliente/Chat/src/pages/PagesChat.jsx
@@ -6,7 +6,7 @@ import { useMyContext } from "../context/Context";
 import { logoutUser } from "../apis/auth";
 import { useNavigate } from "react-router-dom";
 
-const socket = io("chatapp-production-b82e.up.railway.app" || "http://localhost:3000");
+const socket = io(['https://chatapp-production-b82e.up.railway.app/api' , "http://localhost:3000"]);
 
 
 
